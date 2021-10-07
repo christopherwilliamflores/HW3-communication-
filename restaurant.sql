9529f96545214e6a37920c61e5a58106ac9b6e77
Christopher <christopher@MBPdeChistopher.lan>
2021-10-07 18:54:44 -0500

    add sql

---
+++ restaurant.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE restaurant;
+CREATE TABLE menu(
+	nombreplato varchar(255),
+	precio int
+);
+
+
+//Inserts 
+INSERT INTO menu(nombreplato, precio) VALUES("Aji de gallina",30);
+INSERT INTO menu(nombreplato, precio) VALUES("Arroz con pollo",25);
